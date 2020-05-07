bbf22a69b901a96b301c00b0b27df24f7a88e329
Willyan Fontana <willyanfontana@gmail.com>
2020-05-07 19:59:32 -0300

    Adicionado o método cors e a lista de entidades

--- www/server/src/index.js
+++ www/server/src/index.js
@@ -1,15 +1,28 @@
-const Express = require('express');
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
 
 const app = Express();
 
-app.get('/', (request, response) => {
-  return response.json({
-    dev: 'Willyan Fontana',
-    message: 'Iniciando o desenvolvimento da aplicação'
-  })
-})
-
+app.use(cors());
+app.use(express.json);
+app.use(routes);
 
 app.listen(4000, (request, response) => {
   console.log('Api rodando na porta localhost:4000')
 })
+
+/**
+ * Entidades:
+ * Usuários
+ * Aderência com a LGPD
+ */
+
+/**
+ * Funcionalidades:
+ * Registro
+ * Login
+ * Logout
+ * Adicionar novo teste
+ * Listar todos os testes
+ */
